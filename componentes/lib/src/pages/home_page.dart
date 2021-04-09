8c1c7b4a2e3b01d0571d0352a1a17557d6b0bdb5
Normando Ramírez <nramirezd@cbtis236.edu.mx>
2021-04-09 11:22:22 -0500

    69 Navegar ruta estática

--- componentes/lib/src/pages/home_page.dart
+++ componentes/lib/src/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:componentes/src/pages/alert_page.dart';
 import 'package:componentes/src/providers/menu_provider.dart';
 import 'package:componentes/src/utils/icono_string_util.dart';
 import 'package:flutter/material.dart';
@@ -34,7 +35,11 @@ class HomePage extends StatelessWidget {
         title: Text(opt['texto']),
         leading: getIcon(opt['icon']),
         trailing: Icon(Icons.keyboard_arrow_right, color: Colors.blue),
-        onTap: () {},
+        onTap: () {
+          final route = MaterialPageRoute(builder: (context) => AlertPage());
+
+          Navigator.push(context, route);
+        },
       );
 
       opciones..add(widgetTemp)..add(Divider());
