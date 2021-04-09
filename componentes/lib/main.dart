54286ba9c332101067cde4ebb7937a23c151f964
Normando Ramírez <nramirezd@cbtis236.edu.mx>
2021-04-09 13:00:39 -0500

    70 Navegar con rutas con Nombre

--- componentes/lib/main.dart
+++ componentes/lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:componentes/src/pages/home_page.dart';
+import 'package:componentes/src/pages/alert_page.dart';
+import 'package:componentes/src/routes/routes.dart';
 
 import 'package:flutter/material.dart';
 
@@ -8,8 +9,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Componentes APP',
-        debugShowCheckedModeBanner: false,
-        home: HomePage());
+      title: 'Componentes APP',
+      debugShowCheckedModeBanner: false,
+
+      //home: HomePage()
+      //
+      initialRoute: '/',
+      routes: getApplicationRoutes(),
+
+      onGenerateRoute: (RouteSettings settings) {
+        print('Ruta llamada: ${settings.name}');
+
+        return MaterialPageRoute(
+            builder: (BuildContext context) => AlertPage());
+      },
+    );
   }
 }
