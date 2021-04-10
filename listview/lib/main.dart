2c0f58d01fcbec12ddddb9f7855308e8c394dcc8
Normando Ramírez <nramirezd@cbtis236.edu.mx>
2021-04-10 13:04:58 -0500

    ListView

--- listview/lib/main.dart
+++ listview/lib/main.dart
@@ -3,37 +3,125 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Componentes App',
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text('ListView'),
-            ),
-            body: ListView(children: _crearItemsCorta()
-                //children: _crearItems()
-                )));
+      title: 'Material App',
+      home: MyHomePage(),
+    );
   }
-
-  
-
-  final opciones = [
-    "uno",
-    "dos",
-    "tres",
-    "cuatro",
-    'Cinco',
-    'Seis',
-    'Siete',
-    'Ocho',
-    'Nueve',
-    'Diez'
-  ];
-
-
 }
 
+class MyHomePage extends StatelessWidget {
+  List nombres = [
+    "Mario",
+    'Luis',
+    'María',
+    'Pedro',
+    'Luis',
+    'Laura',
+    'Rosa',
+    'Claudia',
+    'FAbiola',
+    'Juan',
+    'Roberto',
+    "Mario",
+    'Luis',
+    'María',
+    'Pedro',
+    'Luis',
+    'Laura',
+    'Rosa',
+    'Claudia',
+    'FAbiola',
+    'Juan',
+    'Roberto'
+  ];
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Material App Bar'),
+      ),
+      body: ListView(
+        children: _crearItemsCorto()
+      ),
+      
+      
+      
+      // body: ListView.builder(
+      //     itemCount: nombres.length,
+      //     itemBuilder: (BuildContext context, int index) {
+      //       final nombre = nombres[index];
+
+      //       return ListTile(
+      //         title: Text(nombre),
+      //         leading: Icon(Icons.person),
+      //         trailing: Icon(Icons.arrow_forward),
+      //         onTap: (){},
+      //       );
+      //     }
+      // ),
+
+      // body: ListView.builder(
+      //   itemBuilder: (BuildContext context, int index){
+      //     return Card(
+      //       child: Image.network('https://thispersondoesnotexist.com/image')
+      //       );
+      //   }
+      // ),
+
+      /*
+      body: ListView(children: [
+        Card(
+          child: Image.network("https://thispersondoesnotexist.com/image")
+        ),
+        Card(
+          child: Image.network("https://thispersondoesnotexist.com/image")
+        ),
+        Card(
+          child: Image.network("https://thispersondoesnotexist.com/image")
+        ),
+      ],)*/
+    );
+  }
+
+  //Forma Larga
+  List<Widget> _crearItems() {
+
+    List<Widget> lista = [];
+
+    for (String nombre in nombres) {
+      final tempWidget = ListTile(
+        title: Text(nombre),
+      );
+
+      lista..add(tempWidget)
+           ..add(Divider());
+    }
+    
+    return lista;
+
+  }
+
+  //Forma Corta
+  List<Widget> _crearItemsCorto(){
+    return nombres.map((nombre) {
+
+      return Column(
+        children: [
+          ListTile(
+            title: Text(nombre),
+            subtitle: Text('SubTema'),
+            leading: Icon(Icons.contact_phone),
+            trailing: Icon(Icons.keyboard_arrow_right),
+            onTap: (){},
+          ),
+          Divider()
+        ],
+      );
+      
+    }).toList();
+  }
+}
